056ddb188adf372f9bbfc7886f1b7c4ce769067f
Bradley Fernandez <99092565+UvuvDev@users.noreply.github.com>
2025-05-15 17:01:01 -0400

    Fix print
    FIxed printing so it would give the correct illegal byte

--- shellcodechecker.py
+++ shellcodechecker.py
@@ -15,7 +15,7 @@ readShellcode = ""
 illegalBytes = int(input("Enter illegal bytes as hex numbers (format 0011223344) - "), base=16)
 illegalBytesCnt = int(input("and also how many you inputted (for the above example, 5) - "))
 
-def compileAndCheckForBytes():
+def compileAndCheckForBytes(line):
 
     shellcode = asm(readShellcode)
 
@@ -23,10 +23,11 @@ def compileAndCheckForBytes():
     for i in range(0, illegalBytesCnt):
         for j in shellcode:
             if j == (byteCheck & 0xFF): # If an illegal byte is seen, return True
-                return True 
+                print(asm(line), end="")
+                return i
         byteCheck = byteCheck >> 8    
         
-    return False
+    return -1
 
 # Open the shellcode and read the full assembly file
 with open(sys.argv[1], 'r') as file:
@@ -43,13 +44,15 @@ with open(sys.argv[1], "r") as file:
         if not line:
             break  # Stop when end of file is reached
 
-        if compileAndCheckForBytes():
-            print("On line " + str(lineNum) + ", this instruction - " + line.strip() +
+        check = compileAndCheckForBytes(line)
+
+        if check != -1:
+            print(" - instruction in hex. On line " + str(lineNum) + ", this instruction - " + line.strip() +
             " - had an illegal byte: " +
-            ' '.join(f'0x{(illegalBytes >> (8 * i)) & 0xFF:02x}' for i in range(illegalBytesCnt)))
+            ' '.join(f'0x{(illegalBytes >> (8 * check)) & 0xFF:02x}' for i in range(1)))
             exit()
 
-        print("E")
+        print("Ok L" + str(lineNum))
     
 
 print("All Clear!")
